ed4d6e920ed06e020b07195da5714bf787ffe1a0
Abraão Reis <andradeabraao23@gmail.com>
2025-10-03 16:51:05 -0300

    chore: integração do dotenv-java para variáveis de ambiente

--- src/main/java/com/squad6/deneasybot/DeneasybotApplication.java
+++ src/main/java/com/squad6/deneasybot/DeneasybotApplication.java
@@ -1,13 +1,19 @@
 package com.squad6.deneasybot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeneasybotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeneasybotApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .load();
+
+        SpringApplication.run(DeneasybotApplication.class, args);
+    }
 
 }
