f3ef7707d042564a43879c381807db719ca3320d
Abraão Reis <andradeabraao23@gmail.com>
2025-10-08 11:08:30 -0300

    chore: removing dotenv config in main class; not needed

--- src/main/java/com/squad6/deneasybot/DeneasybotApplication.java
+++ src/main/java/com/squad6/deneasybot/DeneasybotApplication.java
@@ -1,6 +1,5 @@
 package com.squad6.deneasybot;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeneasybotApplication {
 
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure()
-                .directory(System.getProperty("user.dir"))
-                .load();
-
         SpringApplication.run(DeneasybotApplication.class, args);
     }
 
