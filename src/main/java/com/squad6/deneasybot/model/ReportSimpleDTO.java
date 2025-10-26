c99cdc4e5f36f349964cfd50d10f01cea2377901
JM | José Matheus <Jmatheus_vida@outlook.com>
2025-10-25 21:50:02 -0300

    Update ReportSimpleDTO.java

--- src/main/java/com/squad6/deneasybot/model/ReportSimpleDTO.java
+++ src/main/java/com/squad6/deneasybot/model/ReportSimpleDTO.java
@@ -1,13 +1,16 @@
 package com.squad6.deneasybot.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public record ReportSimpleDTO(
         String reportType,
-        String dates,
-        BigDecimal operatingRevenue,
+        String companyName,
+        LocalDate startDate,
+        LocalDate endDate,
+        BigDecimal operationalRevenue,
         BigDecimal variableCosts,
         BigDecimal fixedExpenses,
-        BigDecimal operatingResult
+        BigDecimal operationalResult
 ) {
 }
