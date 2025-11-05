941ec14626e976d83676ba2f381c9b8668c3bb00
Abraão Reis <andradeabraao23@gmail.com>
2025-11-04 22:52:33 -0300

    feat: user repository func for find by company id

--- src/main/java/com/squad6/deneasybot/exception/DataIntegrityException.java
+++ src/main/java/com/squad6/deneasybot/exception/DataIntegrityException.java
@@ -1,7 +1,7 @@
 package com.squad6.deneasybot.exception;
 
 public class DataIntegrityException extends RuntimeException {
-  public DataIntegrityException(String message) {
-    super(message);
-  }
+    public DataIntegrityException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/squad6/deneasybot/repository/UserRepository.java
+++ src/main/java/com/squad6/deneasybot/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.squad6.deneasybot.repository;
 
 import com.squad6.deneasybot.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySessionToken(String token);
 
     Optional<User> findByPhone(String phone);
+
+    List<User> findAllByCompanyId(Long companyId);
 }
