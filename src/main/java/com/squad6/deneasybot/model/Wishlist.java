af604c6e81a0a5267f95f7a62ca86eb6c3aecd7d
kayky <kaykyb255@gmail.com>
2025-11-12 17:38:26 -0300

    feat: wishlist repository and model

--- src/main/java/com/squad6/deneasybot/model/Wishlist.java
+++ src/main/java/com/squad6/deneasybot/model/Wishlist.java
@@ -1,4 +1,61 @@
 package com.squad6.deneasybot.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wishlist_table")
 public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Wishlist() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/squad6/deneasybot/model/WishlistDTO.java
+++ src/main/java/com/squad6/deneasybot/model/WishlistDTO.java
@@ -1,4 +1,12 @@
 package com.squad6.deneasybot.model;
 
-public record WishlistDTO() {
+import java.time.LocalDateTime;
+
+public record WishlistDTO(
+        Long id,
+        String content,
+        LocalDateTime createdAt,
+        String userName,
+        String companyName
+) {
 }
