961d3dc8dd4776698b6c944f498dcf2624c42f60
fragamateus <mateusnear@hotmail.com>
2025-11-13 22:29:02 -0300

    feat: adiciona query para média de rating no EvaluationRepository

--- src/main/java/com/squad6/deneasybot/repository/EvaluationRepository.java
+++ src/main/java/com/squad6/deneasybot/repository/EvaluationRepository.java
@@ -2,8 +2,12 @@ package com.squad6.deneasybot.repository;
 
 import com.squad6.deneasybot.model.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+
 
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
     List<Evaluation> findAllByOrderByCreatedAtDesc();
+    @Query("SELECT AVG(e.rating) FROM Evaluation e")
+    Double findAverageRating();
 }
