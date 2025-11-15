44eff7f16754f3ef95ef88cc8c7d78cae7cef6b9
SOOSA <soosavinicius@gmail.com>
2025-11-15 19:21:32 -0300

    feat: crud companycontroller

--- src/main/java/com/squad6/deneasybot/controller/CompanyController.java
+++ src/main/java/com/squad6/deneasybot/controller/CompanyController.java
@@ -1,5 +1,6 @@
 package com.squad6.deneasybot.controller;
 
+import com.squad6.deneasybot.model.Company;
 import com.squad6.deneasybot.model.CompanyDTO;
 import com.squad6.deneasybot.service.AuthService;
 import com.squad6.deneasybot.service.CompanyService;
@@ -35,4 +36,24 @@ public class CompanyController {
     public ResponseEntity<CompanyDTO> getCompanyById(@PathVariable Long id) {
         return ResponseEntity.ok(companyService.getCompanyById(id));
     }
+
+    @PostMapping
+    public ResponseEntity<Company> createCompany(@RequestBody CompanyDTO company) {
+        Company newCompany = companyService.createCompany(company);
+        return ResponseEntity.ok(newCompany);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CompanyDTO> updateCompany(@PathVariable Long id, @RequestBody CompanyDTO company) {
+        CompanyDTO updatedDto = companyService.updateCompany(id, company);
+        return ResponseEntity.ok(updatedDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
+        companyService.deleteCompany(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
