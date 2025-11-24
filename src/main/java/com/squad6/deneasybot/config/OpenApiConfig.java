3ffc37261c0af66a5f6b1aa49793f0d618442c91
Rennan <rennan151512@gmail.com>
2025-11-24 01:44:47 -0300

    feat: cors added

--- src/main/java/com/squad6/deneasybot/config/OpenApiConfig.java
+++ src/main/java/com/squad6/deneasybot/config/OpenApiConfig.java
@@ -2,12 +2,10 @@ package com.squad6.deneasybot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
@@ -15,16 +13,9 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("DeneasyBot API")
-                .version("1.0")
+        return new OpenAPI().info(new Info().title("DeneasyBot API").version("1.0")
                 .description("Documentação da API do Assistente Virtual DeneasyBot")
-                .contact(new Contact()
-                        .name("Squad 6")
-                        .email("contact@example.com"))
-                .license(new License()
-                        .name("License Name")
-                        .url("https://example.com/license"))
-        );
+                .contact(new Contact().name("Squad 6").email("abraao.reis@souunit.com.br"))
+                .license(new License().name("License Name").url("https://example.com/license")));
     }
 }
--- src/main/java/com/squad6/deneasybot/config/SecurityConfig.java
+++ src/main/java/com/squad6/deneasybot/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.squad6.deneasybot.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @Profile("dev")
@@ -25,13 +31,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/health").permitAll()
-                        .requestMatchers("/auth/admin/login").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/webhook").permitAll()
-                        .requestMatchers("/h2-console/**").permitAll()
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/auth/admin/login").permitAll().requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/webhook").permitAll().requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -39,6 +42,20 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAdminAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-requested-with", "X-Code"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
