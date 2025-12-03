22004e5aacf26b6ca757bc67b356eea1d92dfa6f
Abraão Reis <andradeabraao23@gmail.com>
2025-12-03 03:55:17 -0300

    feat: services provided dashboard

--- src/main/java/com/squad6/deneasybot/model/DashboardMetricsDTO.java
+++ src/main/java/com/squad6/deneasybot/model/DashboardMetricsDTO.java
@@ -1,9 +1,12 @@
 package com.squad6.deneasybot.model;
 
+import java.util.List;
+
 public record DashboardMetricsDTO(
         long totalCompanies,
         long totalUsers,
         long totalFeedbacks,
-        Double averageRating
+        Double averageRating,
+        List<ChartDataDTO> attendanceHistory
 ) {
 }
